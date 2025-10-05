7a8e37fd4496367358b59bc167727898044a630e
Your Name <your.email@example.com>
2025-10-05 12:54:42 +0530

    ggg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import "./Navbar.css";
+import "./Navbar.css"; 
 
 const Navbar = () => {
   const navLinks = [
